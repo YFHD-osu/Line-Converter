d05cb2934ca5ade5dbc62c8b25af90601dbc9de1
YFHD <92370642+YFHD-osu@users.noreply.github.com>
2024-01-30 00:06:34 +0800

    Add docs

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
 flutter build web --dart-define=BROWSER_IMAGE_DECODING_ENABLED=false --release --base-href "/master/" --web-renderer canvaskit
+del "docs\*.*" /F /Q
+XCOPY build\web\*.* docs\ /E /H /C /Y
