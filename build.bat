e7ec0b2c3d9dfde26f7bc69f5341a10741fe5f86
YFHD <92370642+YFHD-osu@users.noreply.github.com>
2024-02-07 15:12:13 +0800

    Update workflow

--- build.bat
+++
@@ -1,4 +0,0 @@
-flutter build web --dart-define=BROWSER_IMAGE_DECODING_ENABLED=false --release --base-href "/Line-Converter/" --web-renderer canvaskit
-del "docs\*.*" /F /Q
-XCOPY build\web\*.* docs\ /E /H /C /Y
-pause
